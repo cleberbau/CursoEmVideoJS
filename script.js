6e806d5a5bd1ea9a85eec155a237c2f6b90ba0a7
Cleber Pereira da Silva <97200642+cleberbau@users.noreply.github.com>
2022-06-13 08:11:33 -0300

    Aula 5
    Aulas riaveis

---
+++ script.js
@@ -0,0 +1,25 @@
+/*Aula 4
+window.alert('Minha Primeira Mensagem!')
+window.alert('Esta Dando Certo')
+window.prompt('Qual o seu nome?') // 
+window.confirm('Você esta Gostando do video?')
+*/
+
+
+/*Aula 5 – Variáveis e Tipos Primitivos
+var n1 = 5 // Number
+var n2 = 'JavaScript' //String
+var n3 = true //Boolean
+var n4 = undefined  //
+var n5 = null //
+var n6 = 3.14 //
+
+typeof n2
+string*/
+
+
+
+
+
+
+
