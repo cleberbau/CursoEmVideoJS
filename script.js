17e28848722b2165926c9e35e679828f6bad8e55
Cleber Pereira da Silva <97200642+cleberbau@users.noreply.github.com>
2022-06-14 08:32:03 -0300

    Aula 6 – Tratamento de dados
    Você já aprendeu a manipular dados em JavaScript? Sabe como guardar o resultado de um prompt dentro de uma variável? Sabe converter String para Número em JavaScript? Consegue formatar um número para que ele se pareça com um valor monetário usando JavaScript?

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-/*Aula 4
-window.alert('Minha Primeira Mensagem!')
+//CURSO EM VIDEO DE JAVASCRIPT COM GUSTAVO GUANABARA
+
+//Aula 4
+
+/*window.alert('Minha Primeira Mensagem!')
 window.alert('Esta Dando Certo')
 window.prompt('Qual o seu nome?') // 
 window.confirm('Você esta Gostando do video?')
 */
 
 
-/*Aula 5 – Variáveis e Tipos Primitivos
-var n1 = 5 // Number
+//Aula 5 – Variáveis e Tipos Primitivos
+
+/*var n1 = 5 // Number
 var n2 = 'JavaScript' //String
 var n3 = true //Boolean
 var n4 = undefined  //
-var n5 = null //
+var n5 = null // Object
 var n6 = 3.14 //
-
 typeof n2
 string*/
 
+//Aula 6 – Tratamento de dados
+
+/*var nome = window.prompt('Qual é o seu nome?') // Vai perguntar o nome...
+window.alert('É um grande prazer em te conhecer, '+ nome + "!" )//Cocatenação*/
+
+var n1 = Number(window.prompt('Digite um número: '))//Convert string to number
+var n2 = Number(window.prompt('Digite outro número: '))
+var s = n1 + n2
+document.write('A Soma dos valores é' + s)
+var s1 = string(window.prompt('Digite seu Nome: '))// Convert number to
+
+.toUpperCase()// Deixa as Palavras Grandes
+.toLowerCase()// Deixa as Palvras Pequenas
+.toFixed()// Estipula a Quantidade de Casas decimais
+.length()// Conta os Caracteres
+.replace('.', ',')// Substitui um item por outro
+.toLocaleString('pt-BR',(style:'currency',currency:'BRL'))// Converte para Moeda Local
 
 
 
