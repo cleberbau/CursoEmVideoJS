84f4ce800824926ca836967f64cb2e10985d3c6c
Cleber Pereira da Silva <97200642+cleberbau@users.noreply.github.com>
2022-06-20 08:00:38 -0300

    Aula 11 - Condições
    Você sabe usar o comando if no JavaScript? Sabe diferenciar condições simples e condições compostas? Sabe como criar uma condição usando JavaScript? Sabe como instalar e configurar a extensão Node Exec no Visual Studio Code?

---
+++ Aula 11 Condições.js
@@ -0,0 +1,15 @@
+/*var vel = 60.1
+console.log(`A Velocidade do seu carro é ${vel} Km/h`)
+if(vel > 60){// Condição simples!
+    console.log (`Você utrapassou a Velocidade Permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)*/
+
+var pais = 'Brasil'.toUpperCase()
+console.log(`Vivendo em ${pais}`)
+if (pais != 'BRASIL'){
+    console.log('Você é Gringo')
+}else{
+    console.log('Você é Sofredor!')
+}
