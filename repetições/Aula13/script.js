af51204a413f40ab91c757c4241ed86cda9c5f37
Cleber Pereira da Silva <97200642+cleberbau@users.noreply.github.com>
2022-06-29 07:31:56 -0300

    Aula 14
    Apresentação do Laço FOR  e depuração

---
+++ repetições/Aula13/script.js
@@ -0,0 +1,15 @@
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)*/
+
+//AULA 14
+console.log('Vai Começar...')
+for (var c = 1;c <=5 ;c++ ){
+    console.log(c)
+}
+console.log('fim')
+
+
+   
