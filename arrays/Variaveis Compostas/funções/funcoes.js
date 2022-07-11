9c5edf3e17d6b7a4da8074048f39cc632a6c6b2d
Cleber Pereira da Silva <97200642+cleberbau@users.noreply.github.com>
2022-07-11 07:53:32 -0300

    Curso em Video - Exercicios Final

---
+++ arrays/Variaveis Compostas/funções/funcoes.js
@@ -0,0 +1,15 @@
+function parimp(n){
+    if (n%2 == 0){
+        console.log(`O Numero ${n} é Par`)
+    } else {
+        console.log(`O Numero ${n} é Impar`)
+    }
+}
+parimp(302)
+
+
+function soma (n1, n2){
+    let n = n1 + n2
+    console.log(n)
+}
+soma(1,2)
